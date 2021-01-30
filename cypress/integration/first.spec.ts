6fc2b77364f06913b4d234eaaaca7b89ea6b53e5
pyamposlky <pavel.yampolsky@williamhill.com>
2021-01-30 21:55:23 +0100

    check linter setup

--- cypress/integration/first.spec.ts
+++ cypress/integration/first.spec.ts
@@ -1,7 +1,7 @@
 describe('google search', () => {
     it('should work', () => {
         cy.visit('http://www.google.com');
-        //cy.get('#lst-ib').type('Hello world{enter}');
+        cy.clearCookies();
         cy.wait(1000);
     });
 });
